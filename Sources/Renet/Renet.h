549a46709126b76a80724f87983e7ff2f280c464
Lakr Aream <master@233owo.com>
2021-09-18 11:50:58 +0800

    Update

--- Sources/Renet/Renet.h
+++ Sources/Renet/Renet.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SGRenet : NSObject
 
-+ (void)resolveNetworkProblmeForAppWithBundleId:(NSString *)bundleId;
++ (int)resolveNetworkProblemForAppWithBundleId:(NSString *)bundleId;
 
 @end
 
--- Sources/Renet/Renet.m
+++ Sources/Renet/Renet.m
@@ -12,7 +12,7 @@
 
 @implementation SGRenet
 
-+ (void)resolveNetworkProblmeForAppWithBundleId:(NSString *)bundleId {
++ (int)resolveNetworkProblemForAppWithBundleId:(NSString *)bundleId {
 	NSBundle *cellularBundle = [NSBundle bundleWithPath:@"/System/Library/PrivateFrameworks/SettingsCellular.framework"];
 	if (![cellularBundle load]) {
 		cellularBundle = [NSBundle bundleWithPath:@"/System/Library/PrivateFrameworks/Preferences.framework"];
@@ -20,7 +20,7 @@
 			NSLog(@"[+] Preferences.framework is loaded\n");
 		} else {
 			NSLog(@"[-] error: cannot load Preferences.framework\n");
-			return;
+			return EPERM;
 		}
 	} else {
 		NSLog(@"[+] SettingsCellular.framework is loaded\n");
@@ -35,14 +35,17 @@
 		 forBundleIdentifier:bundleId
 		 completionHandler:nil];
 		NSLog(@"[+] setup AppWirelessDataUsageManager\n");
+        return 0;
 	} else if (c(PSAppDataUsagePolicyCache)) {
 		[[c(PSAppDataUsagePolicyCache) sharedInstance] setUsagePoliciesForBundle:bundleId
 		 cellular:YES
 		 wifi:YES];
 		NSLog(@"[+] setup PSAppDataUsagePolicyCache\n");
+        return 0;
 	} else {
 		NSLog(@"[-] error: cannot realize AppWirelessDataUsageManager or PSAppDataUsagePolicyCache\n");
 	}
+    return EPERM;
 }
 
 - (void)setAppWirelessDataOption:(NSNumber *)options forBundleIdentifier:(NSString *)bundleId completionHandler:(id)completionHandler {
